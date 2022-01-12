9619dfdb893c28b9924385a85fa06db3e373348e
Bob Dust <bdust@reynolds.edu>
2022-01-12 12:50:31 -0500

    Add project files.

---
+++ Shapes/TestShapes.cs
@@ -0,0 +1,31 @@
+﻿#define stub
+#undef stub
+using static System.Console;
+
+namespace Shapes
+{
+    public static class TestShapes
+    {
+        public static void Test()
+        {
+#if stub
+            var circle = new Circle(3);
+            ShowShape(circle);
+            var rectangle = new Rectangle(20, 25);
+            ShowShape(rectangle);
+            var square = new Square(18);
+            ShowShape(square);
+            square.Length = 55;
+            ShowShape(square);
+            rectangle = new Rectangle();
+            ShowShape(rectangle);
+#endif
+        }
+#if stub
+        static void ShowShape(Shape shape)
+        {
+            WriteLine($"{shape.GetType().Name}\tArea = {shape.Area}\t\tPerimeter = {shape.Perimeter}\t\tUom = {shape.UoM}");
+        }
+#endif
+    }
+}
---
+++ TestConsole/Program.cs
@@ -0,0 +1,91 @@
+﻿#define Shapes
+#define EmployeePay
+#define Debugging
+#define StudentCollection
+#define StateCollection
+#define Sales
+#define Purchasing
+#define EDI
+#define ETL
+//-----------< Comment the #undef for the project you are testing >-----------//
+#undef Shapes
+#undef EmployeePay
+#undef Debugging
+#undef StudentCollection
+#undef StateCollection
+#undef Sales
+#undef Purchasing
+#undef EDI
+#undef ETL
+
+using static System.Console;
+#if Shapes
+using Shapes;
+#endif
+#if Debugging
+using Debugging;
+#endif
+#if EmployeePay
+using EmployeePay;
+#endif
+#if Purchasing
+using Purchasing;
+#endif
+
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("<----- Student: YOUR NAME GOES HERE ----->");
+#region Shapes
+#if Shapes
+            Shapes.TestShapes.Test();
+#endif
+#endregion
+#region Employee Pay
+#if EmployeePay
+            EmployeePay.EmployeeTest.Test();
+#endif
+#endregion
+#region Debugging
+#if Debugging
+            Debugging.Debugging.Test();
+#endif
+#endregion
+#region Student Collection
+#if StudentCollection
+            Collection.TestStudentCollection.TestStudentCollections();
+#endif
+#endregion
+#region State Collection
+#if StateCollection
+            StateCollection.TestStateCollection.Test();
+#endif
+#endregion
+#region Sales
+#if Sales
+            Sales.SalesTest.Test();
+#endif
+#endregion
+#region Purchasing
+#if Purchasing
+            Purchasing.PurchasingTest.Test();
+#endif
+#endregion
+#region EDI
+#if EDI
+            EDI.TestEDI.Test();
+#endif
+#endregion
+#region ETL
+#if ETL
+            ETL.TestEtl.Test();
+#endif
+#endregion
+            ReadKey();
+        }
+    }
+}
